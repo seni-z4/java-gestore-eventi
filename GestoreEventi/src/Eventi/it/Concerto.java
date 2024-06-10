13da4262af1c95dc5486ca5ed8bbc3732f43c228
seniya <seniyathri@gmail.com>
2024-06-10 09:54:42 +0200

    feat;
    updated

--- GestoreEventi/src/Eventi/it/Concerto.java
+++ GestoreEventi/src/Eventi/it/Concerto.java
@@ -1,53 +1,99 @@
-package Eventi.it;
+//package Eventi.it;
+//
+//import java.time.LocalDate;
+//import java.time.LocalTime;
+//
+//import java.time.LocalTime;
+//import java.time.format.DateTimeFormatter;
+//
+//public class Concerto extends Evento {
+//
+//	private LocalTime ora;
+//	private int prezzo;
+//
+////    public Concert(LocalDate data , LocalTime ora) {
+////        this.ora = ora;
+////        this.prezzo = prezzo;
+////    }
+//
+//	// get & set of time
+//	public LocalTime getTime() {
+//		return ora;
+//	}
+//
+//	public void setTime(LocalTime time) {
+//		this.ora = ora;
+//	}
+//
+//	// get & set of prezzo
+//	public int getPrezzo() {
+//		return prezzo;
+//	}
+//
+//	public void setPrezzo(int prezzo) {
+//		this.prezzo = prezzo;
+//	}
+//
+//	
+//	public String getDataOraFormattata() {
+//		 LocalTime currentTime = LocalTime.now();
+//	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+//	        String formattedTime = currentTime.format(formatter);
+////	        System.out.println("Formatted Current Time: " + formattedTime);
+//	        return formattedTime;
+//    }
+//	
+//	 @Override
+//	    public String toString() {
+//	        return "Hai prenotato alle ore : " + getDataOraFormattata() + " e il prezzo del biglietto e di : " + getPrezzo() + "€" ;
+//	    }
+//
+//
+//	
+//}
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+
+package Eventi.it;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Concerto extends Evento {
 
-	private LocalTime ora;
-	private int prezzo;
+    private LocalTime ora;
+    private int prezzo;
 
-//    public Concert(LocalDate data , LocalTime ora) {
-//        this.ora = ora;
-//        this.prezzo = prezzo;
-//    }
-
-	// get & set of time
-	public LocalTime getTime() {
-		return ora;
-	}
-
-	public void setTime(LocalTime time) {
-		this.ora = ora;
-	}
-
-	// get & set of prezzo
-	public int getPrezzo() {
-		return prezzo;
-	}
-
-	public void setPrezzo(int prezzo) {
-		this.prezzo = prezzo;
-	}
-
-	
-	public String getDataOraFormattata() {
-		 LocalTime currentTime = LocalTime.now();
-	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-	        String formattedTime = currentTime.format(formatter);
-//	        System.out.println("Formatted Current Time: " + formattedTime);
-	        return formattedTime;
+    // Constructor calling Evento constructor
+    public Concerto(int numeroPostiTotali) {
+        super(numeroPostiTotali);
     }
-	
-	 @Override
-	    public String toString() {
-	        return "Hai prenotato alle ore : " + getDataOraFormattata() + " e il prezzo del biglietto e di : " + getPrezzo() + "€" ;
-	    }
 
+    // get & set of time
+    public LocalTime getTime() {
+        return ora;
+    }
 
-	
+    public void setTime(LocalTime time) {
+        this.ora = time;
+    }
+
+    // get & set of prezzo
+    public int getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(int prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getDataOraFormattata() {
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return currentTime.format(formatter);
+    }
+
+    @Override
+    public String toString() {
+        return "Hai prenotato alle ore: " + getDataOraFormattata() + " e il prezzo del biglietto è di: " + getPrezzo() + "€";
+    }
 }
--- GestoreEventi/src/Eventi/it/Main.java
+++ GestoreEventi/src/Eventi/it/Main.java
@@ -1,9 +1,130 @@
+//package Eventi.it;
+//
+//import java.time.Instant;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.time.LocalTime;
+//import java.time.format.DateTimeFormatter;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//
+//		Evento event1 = new Evento(100);
+//		Concerto consert = new Concerto();
+//
+//		Scanner input = new Scanner(System.in);
+//		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+////		event1.setData(LocalDateTime.of(2024, 5, 24, 10, 30));
+////		consert.setTime(LocalTime.of(10, 00));
+//
+//		// titolo
+//		System.out.println("Inserici il titolo del Evento");
+//		event1.setTitolo(input.nextLine());
+//
+//		// data
+//		System.out.println("Inserici la data del evento");
+//		String data = input.nextLine();
+//		LocalDate dataConvertito = LocalDate.parse(data, dateFormatter);
+//
+//		// prezzo del bilgetto
+//		System.out.println("Inserisci il prezzo del biglietto:");
+//		consert.setPrezzo(input.nextInt());
+//
+//		// current time
+//
+////		 LocalTime currentTime = LocalTime.now();
+////	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+////	        String formattedTime = currentTime.format(formatter);
+////	        System.out.println("Formatted Current Time: " + formattedTime);
+//	    
+//
+//		event1.setData(dataConvertito.atStartOfDay());
+//
+//		if (!event1.isValidDate(dataConvertito.atStartOfDay())) {
+//			return;
+//		}
+////		System.out.println(event1.getData());
+//
+//		System.out.println("inserici posti da prenotare : ");
+//		event1.prenota(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//		System.out.println("inserici posti da prenotare : ");
+//		event1.prenota(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println("inserici posti da prenotare : ");
+//		event1.prenota(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println("inserici posti da prenotare : ");
+//		event1.prenota(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println("inserici posti da dirdire : ");
+//		event1.disdici(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println("inserici posti da prenotare : ");
+//		event1.prenota(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println("inserici posti da dirdire : ");
+//		event1.disdici(input.nextInt());
+//		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
+//		System.out.println("posti totali : " + event1.getPostiTotale());
+//
+//		System.out.println();
+//		System.out.println(event1);
+//		System.out.println(consert.toString());
+//
+//
+////		// TODO Auto-generated method stub
+////		Evento event = new Evento();
+////
+////		System.out.println("#####################################################");
+////		Scanner input = new Scanner(System.in);
+//////
+//////		// posti disponibili
+//////		System.out.println("inserici il numero di posto per vedere ci sono posti disponibili");
+//////		event.setPostiTotali(input.nextInt());
+//////
+//////		// posti prenotati
+//////		System.out.println("inserici i posti prenotati");
+//////		event.prenota(input.nextInt());
+////
+////////		//posti disdire
+////////		System.out.println("inserici quanti posti vuoi disdire");
+////////		event.disdici(input.nextInt());
+////
+////		System.out.println("insert the total number of spots");
+////		System.out.println(event.prenota(input.nextInt()));
+////		event.setNumeroPostiTotali(input.nextInt());
+////		
+////		// Print initial bookings
+////		System.out.println("Posti prenotati: " + event.getNumeroPostiPrenotati());
+////
+////		// Make a booking
+////		event.prenota();
+////		System.out.println("Posti prenotati dopo la prenotazione: " + event.getNumeroPostiPrenotati());
+//
+//	}
+//
+//}
+
 package Eventi.it;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
@@ -11,113 +132,52 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Evento event1 = new Evento(100);
-		Concerto consert = new Concerto();
-
 		Scanner input = new Scanner(System.in);
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//		event1.setData(LocalDateTime.of(2024, 5, 24, 10, 30));
-//		consert.setTime(LocalTime.of(10, 00));
 
-		// titolo
-		System.out.println("Inserici il titolo del Evento");
-		event1.setTitolo(input.nextLine());
+		// Create a Concerto object with the total number of seats
+		System.out.println("Inserisci il numero totale di posti:");
+		int numeroPostiTotali = input.nextInt();
+		input.nextLine(); // consume the newline
+		Concerto concerto = new Concerto(numeroPostiTotali);
 
-		// data
-		System.out.println("Inserici la data del evento");
+		// Set the title
+		System.out.println("Inserisci il titolo del concerto:");
+		concerto.setTitolo(input.nextLine());
+
+		// Set the date
+		System.out.println("Inserisci la data del concerto (dd/MM/yyyy):");
 		String data = input.nextLine();
-		LocalDate dataConvertito = LocalDate.parse(data, dateFormatter);
+		LocalDate dataConvertita = LocalDate.parse(data, dateFormatter);
+		concerto.setData(dataConvertita.atStartOfDay());
 
-		// prezzo del bilgetto
-		System.out.println("Inserisci il prezzo del biglietto:");
-		consert.setPrezzo(input.nextInt());
-
-		// current time
-
-//		 LocalTime currentTime = LocalTime.now();
-//	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-//	        String formattedTime = currentTime.format(formatter);
-//	        System.out.println("Formatted Current Time: " + formattedTime);
-	    
-
-		event1.setData(dataConvertito.atStartOfDay());
-
-		if (!event1.isValidDate(dataConvertito.atStartOfDay())) {
+		if (!concerto.isValidDate(dataConvertita.atStartOfDay())) {
+			System.out.println("Data non valida.");
 			return;
 		}
-//		System.out.println(event1.getData());
 
-		System.out.println("inserici posti da prenotare : ");
-		event1.prenota(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
-		System.out.println("inserici posti da prenotare : ");
-		event1.prenota(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
+		// Set the ticket price
+		System.out.println("Inserisci il prezzo del biglietto:");
+		concerto.setPrezzo(input.nextInt());
 
-		System.out.println("inserici posti da prenotare : ");
-		event1.prenota(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
+		// Perform booking operations
+		System.out.println("Inserisci posti da prenotare:");
+		concerto.prenota(input.nextInt());
+		System.out.println("Posti prenotati: " + concerto.getPostiPrenotati());
+		System.out.println("Posti totali disponibili: " + concerto.getPostiTotale());
 
-		System.out.println("inserici posti da prenotare : ");
-		event1.prenota(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
+		System.out.println("Inserisci posti da prenotare:");
+		concerto.prenota(input.nextInt());
+		System.out.println("Posti prenotati: " + concerto.getPostiPrenotati());
+		System.out.println("Posti totali disponibili: " + concerto.getPostiTotale());
 
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
+		System.out.println("Inserisci posti da disdire:");
+		concerto.disdici(input.nextInt());
+		System.out.println("Posti prenotati: " + concerto.getPostiPrenotati());
+		System.out.println("Posti totali disponibili: " + concerto.getPostiTotale());
 
-		System.out.println("inserici posti da dirdire : ");
-		event1.disdici(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
-
-		System.out.println("inserici posti da prenotare : ");
-		event1.prenota(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
-
-		System.out.println("inserici posti da dirdire : ");
-		event1.disdici(input.nextInt());
-		System.out.println("posti prenotati : " + event1.getPostiPrenotati());
-		System.out.println("posti totali : " + event1.getPostiTotale());
-
+		// Print event and concert details
 		System.out.println();
-		System.out.println(event1);
-		System.out.println(consert.toString());
-
-
-//		// TODO Auto-generated method stub
-//		Evento event = new Evento();
-//
-//		System.out.println("#####################################################");
-//		Scanner input = new Scanner(System.in);
-////
-////		// posti disponibili
-////		System.out.println("inserici il numero di posto per vedere ci sono posti disponibili");
-////		event.setPostiTotali(input.nextInt());
-////
-////		// posti prenotati
-////		System.out.println("inserici i posti prenotati");
-////		event.prenota(input.nextInt());
-//
-//////		//posti disdire
-//////		System.out.println("inserici quanti posti vuoi disdire");
-//////		event.disdici(input.nextInt());
-//
-//		System.out.println("insert the total number of spots");
-//		System.out.println(event.prenota(input.nextInt()));
-//		event.setNumeroPostiTotali(input.nextInt());
-//		
-//		// Print initial bookings
-//		System.out.println("Posti prenotati: " + event.getNumeroPostiPrenotati());
-//
-//		// Make a booking
-//		event.prenota();
-//		System.out.println("Posti prenotati dopo la prenotazione: " + event.getNumeroPostiPrenotati());
-
+		System.out.println(concerto);
 	}
-
 }
